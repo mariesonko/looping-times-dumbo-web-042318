aa5dd950be61588c8e6c077101ecab61f783a103
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-17 21:41:25 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,17 @@
+#def using_times
+	#counter = 0
+  #loop do
+  #  counter += 1
+  #  puts "Wingardium Leviosa"
+    #if counter >=7
+    #  break
+    #end
+#end
+#end
+
+#using_times
 def using_times
-	#your code here
-end
-
-
-
-
+  7.times do
+    puts "Wingardium Leviosa"
+  end
+end 
