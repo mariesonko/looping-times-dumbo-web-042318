be7fdbe05f369cffb5bd646f0adfc7bd751b2406
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-17 21:40:06 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,15 +1,17 @@
-def using_times
-	counter = 0
-  loop do 
-    counter += 1
+#def using_times
+	#counter = 0
+  #loop do
+  #  counter += 1
+  #  puts "Wingardium Leviosa"
+    #if counter >=7
+    #  break
+    #end
+#end
+#end
+
+#using_times
+def using_times 
+  7.times do 
     puts "Wingardium Leviosa"
-    if counter >=7
-      break
-    end 
-end
+  end 
 end 
-
-using_times 
-
-
-
